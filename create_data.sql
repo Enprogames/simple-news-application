8c26325a2927be3bfce4c6cbfc1c29ae2a619a1f
ethan <e_posner@outlook.com>
2023-03-27 12:40:43 -0700

    Revised project proposal and data schema

--- create_data.sql
+++ create_data.sql
@@ -1,37 +1,45 @@
 
---  Users(userID, username, password, registerDate)
---  Articles(articleID, title, author, publishDate, content)
---  Comments(commentID, articleID, userID, commentDate, content)
---  Tags(tagID, tagName)
---  ArticleTags(articleID, tagID)
---  Categories(catID, catName)
---  ArticleCategories(catID, articleID)
---  ArticleViews(articleID, userID, viewDate)
+-- UserRoles(roleName, description)
+-- Users(userID, username, password, registerDate)
+-- Articles(articleID, title, author, publishDate, content)
+-- Comments(commentID, articleID, userID, commentDate, content)
+-- Categories(catName, description)
+-- Tags(tagID, tagName, catName)
+-- ArticleTags(articleID, tagID)
+-- ArticleViews(articleID, userID, viewedAt)
 -- 
 -- where
---  - In Comments, articleID references Articles and userID references Users
---  - In ArticleTags, articleID references Articles and tagID references Tags
---  - In ArticleCategories, catID references Categories and articleID references Articles
---  - In ArticleViews, articleID references Articles and userID references Users
+-- In Users, roleName references UserRoles
+-- In Comments, articleID references Articles and userID references Users
+-- In Tags, catName references Categories
+-- In ArticleTags, articleID references Articles and tagID references Tags
+-- In ArticleViews, articleID references Articles and userID references Users
 
--- drop table Users
--- drop table Articles
--- drop table Comments
--- drop table Tags
--- drop table ArticleTags
--- drop table Categories
--- drop table ArticleCategories
 -- drop table ArticleViews
+-- drop table ArticleTags
+-- drop table Tags
+-- drop table Categories
+-- drop table Comments
+-- drop table Articles
+-- drop table Users
+-- drop table UserRoles
 
+-- For this project, this will likely consist soley of "user" and "admin". 
+-- The "admin" role will be able to view reports for what articles/tags/users/categories are most popular/active
+-- The "user" role will be able to view articles (with various sorting options), and comment on them.
+create table UserRoles (
+    roleName varchar(255) primary key,
+    description varchar(1500)
+)
 
 -- Create user with support for sha512 hashed password
--- TODO: Add support for salted passwords
--- TODO: Add support for user email addresses
+-- TODO: Add support for user email addresses?
 create table Users (
     userID char(36) primary key,
     username varchar(255),
-    password char(512),
-    registerDate date
+    password RAW(512),
+    registerDate date,
+    roleName varchar(255) references UserRoles
 );
 
 -- e.g. "How to make a database", "How to make a database in MySQL", "How to make a database in MySQL using SQL"
@@ -53,11 +61,18 @@ create table Comments (
     content text
 );
 
+-- Article categories
+create table Categories (
+    catName varchar(255) primary key,
+    description varchar(1500)
+);
+
 -- Tags for articles
 -- e.g. "database", "sql", "mysql"
 create table Tags (
     tagID char(36) primary key,
     tagName varchar(255),
+		catName varchar(255) references Categories
 );
 
 -- Many to many relationship to show which tags are associated with which articles
@@ -67,28 +82,10 @@ create table ArticleTags (
     tagID char(36) references Tags
 );
 
--- Article categories
-create table Categories (
-    -- TODO: Should this be removed and the catName be made the primary key?
-    catID char(36) primary key,
-    catName varchar(255),
-    -- TODO: description varchar(1500)
-);
-
--- Many to many relationship to show which articles are in which categories
--- TODO: Add unique contraint to ensure that each article is only in each category once?
--- TODO: Should an article only be allowed to have one category?
-create table ArticleCategories (
-    catID char(36) references Categories,
-    articleID char(36) references Articles
-);
-
-
 -- Many to many relationship to show which users have viewed which articles,
 -- and at what times
 create table ArticleViews(
     articleID char(36) references Articles,
     userID char(36) references Users,
-    viewDate date
-    -- TODO: viewTime timestamp
+    viewedAt timestamp
 );
